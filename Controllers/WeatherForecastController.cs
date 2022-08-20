bd6c1c9dd71fe6ac3699d5db27e7aa0e66c18bc7
Helen Alves dos Reis <helen.reis@mrv.com.br>
2022-08-20 16:05:47 -0300

    template inicial aplicação

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KubernetesCanaryDeployment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController()
+        { }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Canary Server";
+        }
+    }
+}
